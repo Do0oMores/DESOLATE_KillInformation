5e713cd56c78742794e23fd50841e6755752b1b9
Do0oMores <do0omores@outlook.com>
2024-09-03 18:35:02 +0800

    RENEW lore: kill track

--- src/main/java/top/mores/PluginCommand/InformationCommand.java
+++ src/main/java/top/mores/PluginCommand/InformationCommand.java
@@ -1,40 +1,60 @@
 package top.mores.PluginCommand;
 
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import top.mores.KillInformation;
+import top.mores.Record.KillTrack;
 import top.mores.Utils.ConfigInformation;
 
 import java.util.List;
 
 public class InformationCommand implements CommandExecutor {
-    ConfigInformation configInformation=new ConfigInformation();
+    ConfigInformation configInformation = new ConfigInformation();
+    KillTrack killTrack = new KillTrack();
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, String[] strings) {
-        if (strings.length == 1 && strings[0].equalsIgnoreCase("reload")) {
-            if (!commandSender.hasPermission("op")) {
-                commandSender.sendMessage("你没有权限执行该命令");
-                return true;
-            }
-            KillInformation.getInstance().reloadConfig();
-            KillInformation.getInstance().reloadDataFile();
-            commandSender.sendMessage(ChatColor.GREEN + "所有配置文件已重新加载");
-        } else if (strings.length == 1 && strings[0].equalsIgnoreCase("cx")) {
-            if (commandSender instanceof Player) {
-                Player player = (Player) commandSender;
-                List<String> messages = configInformation.BuildMessage(player);
-                for (String message : messages) {
-                    player.sendMessage(message);
-                }
-            }
-        } else {
+        if (strings.length != 1) {
             commandSender.sendMessage(ChatColor.RED + "未知的命令");
             return true;
+        }
+        String subCommand = strings[0].toLowerCase();
+
+        switch (subCommand) {
+            case "reload":
+                if (!commandSender.hasPermission("op")) {
+                    commandSender.sendMessage("你没有权限执行该命令");
+                    return true;
+                }
+                KillInformation.getInstance().reloadConfig();
+                KillInformation.getInstance().reloadDataFile();
+                commandSender.sendMessage(ChatColor.GREEN + "所有配置文件已重新加载");
+                break;
+
+            case "cx":
+                if (commandSender instanceof Player player) {
+                    List<String> messages = configInformation.BuildMessage(player);
+                    messages.forEach(player::sendMessage);
+                } else {
+                    commandSender.sendMessage(ChatColor.RED + "该命令只能由玩家执行");
+                }
+                break;
+
+            case "killtrack":
+                if (commandSender instanceof Player player) {
+                    killTrack.initItemLore(player);
+                } else {
+                    commandSender.sendMessage(ChatColor.RED + "该命令只能由玩家执行");
+                }
+                break;
+
+            default:
+                commandSender.sendMessage(ChatColor.RED + "未知的命令");
+                break;
         }
         return true;
     }
---
+++ src/main/java/top/mores/Record/KillTrack.java
@@ -0,0 +1,56 @@
+package top.mores.Record;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KillTrack {
+
+    public void initItemLore(Player player) {
+        ItemStack itemStack = player.getInventory().getItemInMainHand();
+        if (itemStack.getType().equals(Material.AIR)) {
+            player.sendMessage("请手持需要注册击杀追踪的物品");
+            return;
+        }
+        ItemMeta meta = itemStack.getItemMeta();
+        if (meta == null) {
+            player.sendMessage("该物品没有检测到任何标签内容，是否确定注册？请再次输入指令进行注册！");
+            return;
+        }
+        List<String> itemLore = getStrings(meta);
+
+        meta.setLore(itemLore);
+        itemStack.setItemMeta(meta);
+        player.getInventory().setItemInMainHand(itemStack);
+
+        player.sendMessage("击杀记录已更新并替换手中的物品！");
+    }
+
+    @NotNull
+    private static List<String> getStrings(ItemMeta meta) {
+        List<String> itemLore = meta.getLore();
+        if (itemLore == null) {
+            itemLore = new ArrayList<>();
+            itemLore.add("已击杀：0");
+        } else {
+            boolean foundLore = false;
+            for (int i = 0; i < itemLore.size(); i++) {
+                String lore = itemLore.get(i);
+                if (lore.equals("lore")) {
+                    itemLore.set(i, "已击杀：0");
+                    foundLore = true;
+                    break;
+                }
+            }
+            if (!foundLore) {
+                itemLore.add("已击杀：0");
+            }
+        }
+        return itemLore;
+    }
+}
